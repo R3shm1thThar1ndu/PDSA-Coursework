a59ace050010928ded4de0bd346110a8d54adc66
R3shm1thThar1ndu <160410487+R3shm1thThar1ndu@users.noreply.github.com>
2025-08-29 14:48:15 +0530

    Enhance Interest model with Lombok annotations and add new dependencies in pom.xml

--- src/main/java/com/pdsa/touristappbackend/model/Interest.java
+++ src/main/java/com/pdsa/touristappbackend/model/Interest.java
@@ -1,14 +1,26 @@
 package com.pdsa.touristappbackend.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+
 import jakarta.persistence.*;
 
 @Entity
-@Table(name="interests")
+@Table(name = "interests")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Interest {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true, nullable = false)
     private String name;
-}
 
+    public Interest(String name) {
+        this.name = name;
+    }
+}
